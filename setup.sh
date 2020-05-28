d671306f1f4c55321c16fde9ebfd88bb28d9fbfa
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-28 17:34:06 -0500

    Script fixes.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 BOILERPLATE_FILE_NAME=boilerplate.zip
+GITHUB_REPO_NAME=py-boilerplate # change this if forking and renaming
 GITHUB_USERNAME=carlosonunez # change this if forking
 fetch_latest_boilerplate() {
   curl -o "$BOILERPLATE_FILE_NAME" \
-    https://github.com/$GITHUB_USERNAME/new_python_project/archive/latest.zip
+    https://github.com/$GITHUB_USERNAME/$GITHUB_REPO_NAME/archive/latest.zip
 }
 
 unzip_to_current_directory() {
-  unzip "$BOILERPLATE_FILE_NAME" -d ..
+  unzip "$BOILERPLATE_FILE_NAME" -d .
 }
 
 delete_boilerplate_zip() {
