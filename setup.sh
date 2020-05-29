51772d9157c2baa230b4ec59e4855bd44eeac107
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-28 23:47:38 -0500

    Always follow redirects.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 BOILERPLATE_ZIP_URL=https://github.com/carlosonunez/py-boilerplate/archive/latest.zip
 BOILERPLATE_FILE_NAME=boilerplate.zip
 fetch_latest_boilerplate() {
-  curl -o "$BOILERPLATE_FILE_NAME" "$BOILERPLATE_ZIP_URL"
+  curl -Lo "$BOILERPLATE_FILE_NAME" "$BOILERPLATE_ZIP_URL"
 }
 
 unzip_to_current_directory() {
